ce3c5c88a152239dfe8ed81a4a7d1a6989c26a73
Puneet Bajaj <142579024+Puneet-Bajaj-IITM@users.noreply.github.com>
2023-09-13 19:26:16 +0530

    Update app.js
    fixing comments

--- app.js
+++ app.js
@@ -15,18 +15,9 @@ btn.addEventListener("click",()=>{
     task.value ="";
 });
 
-// let X = document.querySelectorAll(".X");
-// for (const btn of X) {
-//     btn.addEventListener("click",()=>{
-//         btn.parentElement.remove();
-//     });
-// }
-//This code doesn't work on new elemnts as new elements added into the list don't get the event listner 
-//for this purpose we use event bubbling
-
 ul.addEventListener("click",(event)=>{
     if(event.target.localName == "button"){
-        event.target.parentElement.remove(); //event.target gives dewscription of the target
+        event.target.parentElement.remove(); //event.target gives description of the target
     }
 })
 
